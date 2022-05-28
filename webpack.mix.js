15934414c8ab1065b4e53d1cb63e552680e033a3
Fityan <fityannugroho@gmail.com>
2022-05-28 20:14:29 +0700

    Install bootstrap css

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,16 +6,15 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 // === Mix configuration for both production and development environments ===
 mix.js('resources/js/app.js', 'public/js');
-mix.postCss('resources/css/app.css', 'public/css', [
-    //
-]);
+mix.sass('resources/sass/app.scss', 'public/css');
+mix.sourceMaps();
 
 // === Mix configuration for development environment ===
 if (!mix.inProduction()) {
