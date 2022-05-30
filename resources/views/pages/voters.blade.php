18429de4a8dd0d63d559e390f90c5e7dcba46cde
Arif Hendrawan Priliyanto <arifhendrawan023@gmail.com>
2022-05-31 00:21:43 +0700

    create  view page voters

--- resources/views/pages/voters.blade.php
+++ resources/views/pages/voters.blade.php
@@ -1,5 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Voters</h1>
+<div class="container">
+    <h2>Voters</h2>
+    <hr>
+    <form>
+        <p class="description-text">Add the participants you want to be able to choose the event you create</p>
+        <div class="form-outline mb-4">
+            <label class="form-label" for="form1Example1">Name <span style="color:red;font-weight:bold">*</span></label>
+            <input type="text" id="form1Example1" class="form-control" placeholder="Enter name participant" required/>
+        </div>
+        <div class="form-outline mb-4">
+            <label class="form-label" for="form2Example1">Email <span style="color:red;font-weight:bold">*</span></label>
+            <input type="email" id="form2Example1" class="form-control" placeholder="Enter email participant" required/>
+        </div>
+        <button type="submit" class="btn btn-primary btn-block" style="width:100%;">ADD PARTICIPANT</button>
+    </form>
+    <h2 class="my-2 py-2">Participant</h2>
+    <div class="d-flex flex-wrap justify-content-around">
+        <div class="card shadow-sm p-3 mb-5 bg-white rounded" style="width: 100%;margin-bottom: 1rem;">
+            <div class="card-body">
+                <p class="card-text">Name : <span>Fast Voting User</span></p>
+                <p class="card-text">Email : <span>fastVoting@xxx.com</span></p>
+                <a href="#" class="btn btn-danger">Delete</a>
+            </div>
+        </div>
+        <div class="card shadow-sm p-3 mb-5 bg-white rounded" style="width: 100%;margin-bottom: 1rem;">
+            <div class="card-body">
+                <p class="card-text">Name : <span>Fast Voting User</span></p>
+                <p class="card-text">Email : <span>fastVoting@xxx.com</span></p>
+                <a href="#" class="btn btn-danger">Delete</a>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
