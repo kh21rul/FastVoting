9eb0b05c81cf38c72841eb0407d0e3aea6ab6ded
Arif Hendrawan Priliyanto <arifhendrawan023@gmail.com>
2022-05-30 23:18:05 +0700

    add background color and padding

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -19,27 +19,27 @@
             </div>
         </div>
     </div> --}}
-    <h2>Welcome <span>UserName</span></h2>
+    <h2>Welcome <span>{{ Auth::user()->name }}</span></h2>
     <div class="d-flex justify-content-start">
         <a type="button" class="btn btn-primary" href="{{route('login')}}" style="margin-right: 2%">Add Vote</a>
         <a type="button" class="btn btn-light border border-dark" href="#learnFastVoting">Log Out</a>
     </div>
     <div class="d-flex flex-wrap justify-content-around">
-        <div class="card my-2" style="width: 100%;">
+        <div class="card my-2" style="width: 100%; box-shadow: 1px 1px 7px rgba(0, 0, 0, 0.25);">
             <div class="card-body">
               <h5 class="card-title">Lorem Ipsum</h5>
               <p class="card-text"><span><img class="img-fluid" src="{{asset('assets/Calendar.png')}}" alt=""></span> 25 June 2022</p>
               <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sem ligula, dapibus nec odio facilisis, facilisis bibendum leo. Quisque laoreet vitae purus nec auctor.</p>
             </div>
         </div>
-        <div class="card my-2" style="width: 100%;">
+        <div class="card my-2" style="width: 100%; box-shadow: 1px 1px 7px rgba(0, 0, 0, 0.25);">
             <div class="card-body">
               <h5 class="card-title">Lorem Ipsum</h5>
               <p class="card-text"><span><img class="img-fluid" src="{{asset('assets/Calendar.png')}}" alt=""></span> 25 June 2022</p>
               <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sem ligula, dapibus nec odio facilisis, facilisis bibendum leo. Quisque laoreet vitae purus nec auctor.</p>
             </div>
         </div>
-        <div class="card my-2" style="width: 100%;">
+        <div class="card my-2" style="width: 100%; box-shadow: 1px 1px 7px rgba(0, 0, 0, 0.25);">
             <div class="card-body">
               <h5 class="card-title">Lorem Ipsum</h5>
               <p class="card-text"><span><img class="img-fluid" src="{{asset('assets/Calendar.png')}}" alt=""></span> 25 June 2022</p>
