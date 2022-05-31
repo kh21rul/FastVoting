53de41490eff021179ec3b20d29bd8bfd50d0ce4
Fityan <fityannugroho@gmail.com>
2022-05-31 13:39:47 +0700

    Create `options` table

---
+++ database/migrations/2022_05_31_063750_create_options_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('options', function (Blueprint $table) {
+            $table->uuid('id')->primary()->unique();
+            $table->foreignUuid('event_id')->references('id')->on('events');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('image_location')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('options');
+    }
+};
