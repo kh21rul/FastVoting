a2ec217ac389053aa17bf4683245e10545baf5ae
Fityan <fityannugroho@gmail.com>
2022-05-31 13:52:13 +0700

    Create `voters` table

---
+++ database/migrations/2022_05_31_064030_create_voters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('voters', function (Blueprint $table) {
+            $table->uuid('id')->primary()->unique();
+            $table->foreignUuid('event_id')->references('id')->on('events');
+            $table->string('name');
+            $table->string('email');
+            $table->string('token');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('voters');
+    }
+};
