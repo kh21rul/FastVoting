cc0ab30913cebf85dc172fe13c07f238613a3b72
Fityan <fityannugroho@gmail.com>
2022-05-31 13:37:05 +0700

    Create `events` table

---
+++ database/migrations/2022_05_31_063047_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->uuid('id')->primary()->unique();
+            $table->foreignUuid('user_id')->references('id')->on('users');
+            $table->string('title');
+            $table->text('description');
+            $table->timestamp('started_at')->nullable();
+            $table->timestamp('finished_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
