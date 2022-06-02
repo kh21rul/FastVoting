a9898518ee9ac216328b2eb40185755146c6d75e
Fityan <fityannugroho@gmail.com>
2022-06-02 20:25:37 +0700

    Revert "Load event list in Dashboard page"
    This reverts commit c0c34b72f9cf1afe11d80c1d89ed3ee8eb4f1bc0.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,18 +13,16 @@ class DashboardController extends Controller
      */
     public function __construct()
     {
-        //
+        $this->middleware(['auth', 'verified']);
     }
 
     /**
      * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
     {
-        $user = auth()->user();
-        $data['title'] = 'Dashboard | '. config('app.name');
-        $data['events'] = $user->events;
-
-        return view('pages.dashboard', $data);
+        return view('pages.dashboard');
     }
 }
