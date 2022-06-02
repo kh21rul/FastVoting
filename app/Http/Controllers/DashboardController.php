c0c34b72f9cf1afe11d80c1d89ed3ee8eb4f1bc0
Fityan <fityannugroho@gmail.com>
2022-06-02 13:34:12 +0700

    Load event list in Dashboard page
    - Get event list from model.
    - Link `Add Event` button to Add New Event page.
    - Link event item to Detail Event page.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,16 +13,18 @@ class DashboardController extends Controller
      */
     public function __construct()
     {
-        $this->middleware(['auth', 'verified']);
+        //
     }
 
     /**
      * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
     {
-        return view('pages.dashboard');
+        $user = auth()->user();
+        $data['title'] = 'Dashboard | '. config('app.name');
+        $data['events'] = $user->events;
+
+        return view('pages.dashboard', $data);
     }
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+<div class="container py-4">
+    <div class="mb-3 d-flex justify-content-end">
+        <a class="btn btn-primary" href="{{ route('event.add') }}">Add Event</a>
+    </div>
+    <div class="d-flex flex-wrap justify-content-around">
+        {{-- Load each `event` in `events` --}}
+        @foreach ($events as $event)
+            <a href="{{ route('event.detail', ['id' => $event->id])}}" class="card my-2 w-100">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <h5 class="card-title">{{ $event->title }}</h5>
+                    <b>{{ __('Started at: ') }}</b>
+                    <span>{{ $event->started_at }}</span>
+                    <br>
+                    <b>{{ __('Finished at: ') }}</b>
+                    <span>{{ $event->finished_at }}</span>
 
-                    {{ __('You are logged in!') }}
+                    {{-- Load event's `description` if not empty --}}
+                    @if ($event->description)
+                        <p class="card-text">{{ $event->description }}</p>
+                    @endif
                 </div>
-            </div>
-        </div>
+            </a>
+        @endforeach
     </div>
 </div>
 @endsection
