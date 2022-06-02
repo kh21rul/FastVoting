f555acd51bfd9ca7cc2a98cb57f4edd0d4d2ee9d
Khairul Aqram <khairulaqram21@gmail.com>
2022-06-02 16:13:14 +0700

    memisahkan template navbar & footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="w-100 mt-auto d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="d-flex align-items-center text-center mx-auto">
+        <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+            <img src="{{ asset('assets/logo.png') }}" class="bi" width="30" height="24">
+        </a>
+        <span class="text-muted">&copy; 2022 FastVoting</span>
+    </div>
+</footer>
