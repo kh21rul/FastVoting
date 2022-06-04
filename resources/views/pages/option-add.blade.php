3536fc39556c505ef82c0d80dea5b9431caaacab
Khairul Aqram <khairulaqram21@gmail.com>
2022-06-04 11:05:40 +0700

    menambahkan input file untuk foto

--- resources/views/pages/option-add.blade.php
+++ resources/views/pages/option-add.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="col-lg-8">
-    <form method="post" action="/events/options">
+    <form method="post" action="/events/options" enctype="multipart/form-data">
         @csrf
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
@@ -13,6 +13,11 @@
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
             <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+        </div>
+        
+        <div class="mb-3">
+            <label for="image_location" class="form-label">Foto</label>
+            <input class="form-control" type="file" id="image_location" name="image_location">
         </div>
         <button type="submit" class="btn btn-primary">add option</button>
     </form>
