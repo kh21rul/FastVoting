980d1060fe973326cd3204ea0cc2eba7acdeaca4
Physco <physcosea11@gmail.com>
2022-06-04 14:47:41 +0700

    add route to button add event

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <h2>Welcome <span>{{ Auth::user()->name }}</span></h2>
     <div class="d-flex justify-content-start">
-        <a type="button" class="btn btn-primary" href="{{route('login')}}" style="margin-right: 2%">Add Vote</a>
+        <a type="button" class="btn btn-primary" href="{{route('event.add')}}" style="margin-right: 2%">Add Vote</a>
     </div>
     <div class="d-flex flex-wrap justify-content-around">
         <div class="card my-2" style="width: 100%; box-shadow: 1px 1px 7px rgba(0, 0, 0, 0.25);">
