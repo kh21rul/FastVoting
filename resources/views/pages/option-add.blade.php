79bbfb8f123c026eaf6918a210cf933489ba639c
Fityan <fityannugroho@gmail.com>
2022-06-06 08:51:37 +0700

    Init add new option feature

--- resources/views/pages/option-add.blade.php
+++ resources/views/pages/option-add.blade.php
@@ -1,25 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div class="col-lg-8">
-    <form method="post" action="/events/options" enctype="multipart/form-data">
+<div class="container py-4">
+    <section style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('event.detail', ['id' => $event->id]) }}">Event</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Add New Option</li>
+        </ol>
+    </section>
+    <h1>Add New Option</h1>
+    <form method="post" action="{{ route('option.create', ['id' => $event->id]) }}" enctype="multipart/form-data">
         @csrf
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control" id="name" name="name">
+            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}">
+            @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
-        
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
-            <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+            <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{ old('description') }}</textarea>
+            @error('description')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
-        
         <div class="mb-3">
-            <label for="image_location" class="form-label">Foto</label>
-            <input class="form-control" type="file" id="image_location" name="image_location">
+            <label for="image" class="form-label">Image</label>
+            <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image">
+             @error('image')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
-        <button type="submit" class="btn btn-primary">add option</button>
+        <button type="submit" class="btn btn-primary">Add Option</button>
     </form>
 </div>
 
