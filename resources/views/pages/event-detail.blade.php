fa2681b57dfceab4d2a2f2e1aed6e78a4ff2eba5
Arif Hendrawan Priliyanto <arifhendrawan023@gmail.com>
2022-06-06 08:06:57 +0700

    improve card in page add event and detail event

--- resources/views/pages/event-detail.blade.php
+++ resources/views/pages/event-detail.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <h2 class="detail-event">Lorem Ipsum</h2>
     <div class="d-flex justify-content-start my-2">
-        <a type="button" class="btn btn-success" style="margin-right:1%;" href="#">Edit Event</a>
+        <a type="button" class="btn btn-success" style="margin-right:1%;" href="{{'add'}}">Edit Event</a>
         <a type="button" class="btn btn-danger" href="#">Delete Event</a>
     </div>
     <p class="card-text"><span><img class="img-fluid" src="{{asset('assets/Calendar.png')}}" alt=""></span> 25 June 2022 at 8 AM - 25 June 2022 at 5 PM</p>
@@ -20,21 +20,21 @@
         <h3>Choice :</h3>
         <div class="d-flex flex-wrap choiceCard">
             <div class="card choice" style="margin-bottom: 1rem;">
-                <div class="card-head d-flex justify-content-center p-2"><img src="{{asset('assets/imgCandidate.png')}}" alt="" style="width: 90%"></div>
+                <div class="card-head d-flex justify-content-center p-2" style="height: 75%"><img src="{{asset('assets/imgCandidate.png')}}" alt="" style="max-width: 100%"></div>
                 <div class="card-body">
                   <h5 class="card-title text-center">Special title treatment</h5>
                   <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 </div>
             </div>
             <div class="card choice" style="margin-bottom: 1rem;">
-                <div class="card-head d-flex justify-content-center p-2"><img src="{{asset('assets/imgCandidate.png')}}" alt="" style="width: 90%"></div>
+                <div class="card-head d-flex justify-content-center p-2" style="height: 75%"><img src="{{asset('assets/imgCandidate.png')}}" alt="" style="max-width: 100%"></div>
                 <div class="card-body">
                   <h5 class="card-title text-center">Special title treatment</h5>
                   <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 </div>
             </div>
             <div class="card choice" style="margin-bottom: 1rem;">
-                <div class="card-head d-flex justify-content-center p-2"><img src="{{asset('assets/imgCandidate.png')}}" alt="" style="width: 90%"></div>
+                <div class="card-head d-flex justify-content-center p-2" style="height: 75%"><img src="{{asset('assets/imgCandidate.png')}}" alt="" style="max-width: 100%"></div>
                 <div class="card-body">
                   <h5 class="card-title text-center">Special title treatment</h5>
                   <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
