ff4dc4191872d04051e38f51313a1fe50ed6ae7a
Arif Hendrawan Priliyanto <arifhendrawan023@gmail.com>
2022-06-10 21:42:44 +0700

    fix card & overflow img in result page

--- resources/views/pages/result.blade.php
+++ resources/views/pages/result.blade.php
@@ -30,28 +30,34 @@
         </tbody>
     </table>
         <div class="d-flex flex-wrap choiceCard">
-            <div class="card choice" style="margin-bottom: 1rem;">
-                <div class="card-head d-flex justify-content-center p-2" style="height: 75%"><img src="{{asset('assets/imgCandidate.png')}}" alt="" style="max-width: 100%"></div>
+            <div class="card me-3" style="margin-bottom: 1rem; width: 18rem;">
+                <div class="cropped">
+                    <img src="{{asset('assets/imgCandidate.png')}}" alt="">
+                </div>
                 <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
-                  <p class="card-text mb-1">Total Vote : 250</p>
-                  <p class="card-text">Percentage who voted : 25 %</p>
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text mb-1">Total Vote : 250</p>
+                    <p class="card-text">Percentage who voted : 25 %</p>
                 </div>
             </div>
-            <div class="card choice" style="margin-bottom: 1rem;">
-                <div class="card-head d-flex justify-content-center p-2" style="height: 75%"><img src="{{asset('assets/imgCandidate.png')}}" alt="" style="max-width: 100%"></div>
+            <div class="card me-3" style="margin-bottom: 1rem; width: 18rem;">
+                <div class="cropped">
+                    <img src="{{asset('assets/imgCandidate.png')}}" alt="">
+                </div>
                 <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
-                  <p class="card-text mb-1">Total Vote : 250</p>
-                  <p class="card-text">Percentage who voted : 25 %</p>
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text mb-1">Total Vote : 250</p>
+                    <p class="card-text">Percentage who voted : 25 %</p>
                 </div>
             </div>
-            <div class="card choice" style="margin-bottom: 1rem;">
-                <div class="card-head d-flex justify-content-center p-2" style="height: 75%"><img src="{{asset('assets/imgCandidate.png')}}" alt="" style="max-width: 100%"></div>
+            <div class="card me-3" style="margin-bottom: 1rem; width: 18rem;">
+                <div class="cropped">
+                    <img src="{{asset('assets/imgCandidate.png')}}" alt="">
+                </div>
                 <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
-                  <p class="card-text mb-1">Total Vote : 250</p>
-                  <p class="card-text">Percentage who voted : 25 %</p>
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text mb-1">Total Vote : 250</p>
+                    <p class="card-text">Percentage who voted : 25 %</p>
                 </div>
             </div>
         </div>
