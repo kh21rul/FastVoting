539c4314c4d189f070eaa1cee759394b79b53d7e
Fityan <fityannugroho@gmail.com>
2022-06-12 12:48:41 +0700

    Optimize option item structure

--- resources/views/pages/vote.blade.php
+++ resources/views/pages/vote.blade.php
@@ -17,6 +17,7 @@
         <p class="fs-6">{{ $voter->event->description }}</p>
     @endisset
     <hr class="mb-4">
+
     <h2>Your Identity</h2>
     <table class="table table-borderless mb-4">
         <tbody>
@@ -30,46 +31,29 @@
             </tr>
         </tbody>
     </table>
+
+    {{-- Option List --}}
     <div class="option-list">
         @foreach ($voter->event->options as $option)
             <div class="card option-item">
-                @if (isset($option->image_location))
-                    <div class="row g-0">
-                        <div class="col-8">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $option->name }}</h5>
-                                @isset($option->description)
-                                    <p class="card-text">{{ $option->description }}</p>
-                                @endisset
-                                <a class="btn btn-success" href="{{'/events/eventId/result'}}">Vote</a>
-                            </div>
+                <div class="row g-0">
+                    <div class="@isset($option->image_location) col-8 @endisset">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $option->name }}</h5>
+                            @isset($option->description)
+                                <p class="card-text">{{ $option->description }}</p>
+                            @endisset
+                            <a class="btn btn-success" href="{{'/events/eventId/result'}}">Vote</a>
                         </div>
+                    </div>
+                    {{-- Option Image --}}
+                    @isset($option->image_location)
                         <div class="col-4 option-item__image-frame">
                             <img class="option-item__image" src="{{ route('option.image', ['name' => $option->image_location]) }}" alt="{{ $option->name }}">
                         </div>
-                    </div>
-                @else
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $option->name }}</h5>
-                        @isset($option->description)
-                            <p class="card-text">{{ $option->description }}</p>
-                        @endisset
-                        <a class="btn btn-success" href="{{'/events/eventId/result'}}">Vote</a>
-                    </div>
-                @endif
-            </div>
-            {{-- <div class="card option-item">
-                @isset($option->image_location)
-                    <img class="option-item__image" src="{{ route('option.image', ['name' => $option->image_location]) }}" alt="{{ $option->name }}">
-                @endisset
-                <div class="card-body">
-                    <p class="h5 card-title text-center fw-bold">{{ $option->name }}</p>
-                    @isset($option->description)
-                        <p class="card-text">{{ $option->description }}</p>
                     @endisset
-                    <a class="btn btn-success" href="{{'/events/eventId/result'}}">Vote</a>
                 </div>
-            </div> --}}
+            </div>
         @endforeach
     </div>
 </div>
