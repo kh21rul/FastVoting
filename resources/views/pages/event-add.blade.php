6fdac9b5d31a931ae54020c15044fb834ddba681
Fityan <fityannugroho@gmail.com>
2022-06-13 11:05:40 +0700

    Remove unused jquery

--- resources/views/pages/event-add.blade.php
+++ resources/views/pages/event-add.blade.php
@@ -61,27 +61,4 @@
         <button type="submit" class="btn btn-primary">{{ __('Add Event') }}</button>
     </form>
 </div>
-{{--
-<script>
-        $("#close-date").change(function () {
-            const startDate = document.getElementById("start-date").value;
-            const endDate = document.getElementById("close-date").value;
-
-            if ((Date.parse(startDate) > Date.parse(endDate))) {
-                alert("Close date should be greater than Start date");
-                document.getElementById("close-date").value = "";
-            }
-        });
-
-        $("#close-time").change(function () {
-            const startTime = document.getElementById("start-time").value;
-            const endTime = document.getElementById("close-t  ime").value;
-
-            if(startTime>endTime){
-                    alert('start time should be smaller');
-                    document.getElementById("close-time").value = "";
-                }
-        });
-</script>
---}}
 @endsection
