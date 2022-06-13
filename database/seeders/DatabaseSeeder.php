5ca8fa4c0cd34b1629bcab63bc70f8c82f01744f
Fityan <fityannugroho@gmail.com>
2022-06-13 19:05:42 +0700

    Create events seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,12 @@ class DatabaseSeeder extends Seeder
 
         // Create 5 random users.
         \App\Models\User::factory(5)->create();
+
+        // Create an event by The Tester.
+        $this->createEvent($theTester->id);
+
+        // Create 2 random events with random creators.
+        $this->createEvents();
     }
 
     /**
@@ -36,5 +42,30 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt(config('seeder.tester_password')),
             'is_admin' => config('seeder.tester_is_admin'),
         ]);
+    }
+
+    /**
+     * Create some events.
+     * If `userId` is not specified, the event's creator will be picked randomly.
+     *
+     * @param int $amount The amount of events to create.
+     * @param string $userId The user's ID.
+     */
+    private function createEvents(int $amount = 2, string $userId = '')
+    {
+        return \App\Models\Event::factory($amount)->create(
+            $userId ? ['user_id' => $userId] : []
+        );
+    }
+
+    /**
+     * Create an event.
+     * If `userId` is not specified, the event's creator will be picked randomly.
+     *
+     * @param string $userId The user's ID.
+     */
+    private function createEvent(string $userId = '')
+    {
+        return $this->createEvents(1, $userId);
     }
 }
