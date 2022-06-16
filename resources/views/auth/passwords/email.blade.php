745ad63eb36acef50512ca6e708a9b6c5759866f
Fityan <fityannugroho@gmail.com>
2022-06-16 14:01:59 +0700

    Improve verify email and reset password page

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,14 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="d-flex flex-row">
-        <div class="pt-2"><img class="img-fluid py-2" src="{{asset('assets/iconKunci.png')}}" alt="iconLock"></div>
-        <div class="p-2">
-            <h1 style="font-size: 2em;">Forgot Password ?</h1>
-        </div>
+<div class="container py-4">
+    <div class="d-flex align-items-center gap-2 mb-3">
+        <i class="fa-lock fa-solid fa-xl"></i>
+        <h1 class="m-0">Reset Password</h1>
     </div>
-    <p class="description-text">Please enter the email address you used to create account and well send you al link to reset password</p>
+    <p class="">Please enter your registered email and we will send a link to reset your password.</p>
     <hr class="ruler">
     @if (session('status'))
         <div class="alert alert-success" role="alert">
@@ -17,9 +15,12 @@
     @endif
     <form method="POST" action="{{ route('password.email') }}">
         @csrf
-        <div class="form-outline mb-4">
-            <label for="email" class="form-label">{{ __('Email Address') }}<span style="color:red;font-weight:bold"> *</label>
-            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Enter your email" required autocomplete="email" autofocus>
+        <div class="form-outline mb-3">
+            <label for="email" class="form-label">
+                <span>{{ __('Email Address') }}</span>
+                <span class="text-danger fw-bold">*</span>
+            </label>
+            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="your@email.xyz" required autocomplete="email" autofocus>
             @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
