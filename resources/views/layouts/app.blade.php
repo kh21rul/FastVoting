ddb4d530a70772fd6c1ecfdf6425d65fffb7e880
Fityan <fityannugroho@gmail.com>
2022-06-22 10:45:24 +0700

    Add theme color meta tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#0d6efd">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
