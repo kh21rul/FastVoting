7c026b3fd71963635528b4f25f57b5d074cc6f28
Arif Hendrawan Priliyanto <arifhendrawan023@gmail.com>
2022-06-27 20:32:43 +0700

    make section profile team dev

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="container py-4">
-    <h2 class="text-center">About FastVoting</h2>
-    <p class="text-center font-weight-light p-2" style="font-size: 1.35em;">" We can all agree on the importance of voting. "</p>
+    <h2 class="mb-3 text-center">About FastVoting</h2>
+    <p class="subtitle text-center">" We can all agree on the importance of voting. "</p>
     <div class="d-flex about">
         <p class="descriptionAbout" style="text-align: justify;">Voting is a general term that refers to a decision-making mechanism or giving a mandate to someone that can be carried out openly or secretly (confidential). If the voting is carried out openly, it is enough for the parties who have an interest to raise their hands and the numbers will be counted.
             However, if it is imposed in secrecy, voters who have the right have to vote or tick their choice in the voting booth , then put it in the ballot box, and finally the number is counted. Voting is one of the chosen mechanisms in running democracy.
@@ -12,7 +12,98 @@
             With these problems, we took the initiative to solve existing problems by building a website to conduct voting that is safe and free from fraud.
             Fastvoting is a website-based online voting application that can help you organize events to make decisions easily, quickly and avoid fraud, only people who are invited can vote and each account can only vote once. Voters can choose whoever they want wherever they are without queuing for a time limit determined by the event maker (the committee) with your respective gadgets, be it desktop or mobile.</p>
     </div>
-    <img src="{{asset('assets/pexels-element-digital-1550337.jpg')}}" alt="" style="width:100%;">
+    <div class="py-5 team4">
+        <div class="container">
+            <div class="row justify-content-center mb-4">
+                <div class="col-md-7 text-center">
+                <h2 class="mb-3">Experienced & Professional Team</h2>
+                <p class="subtitle">You can relay on our amazing features list and also our customer services will be great experience for you without doubt and in no-time</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 mb-4">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img src="{{ asset('assets/IMG20210522163312.jpg')}}" alt="wrapkit" class="img-fluid rounded-circle"/>
+                        </div>
+                        <div class="col-md-12 text-center">
+                            <div class="pt-2">
+                                <h5 class="mt-4 font-weight-medium mb-0">Arif Hendrawan Priliyanto</h5>
+                                <h6 class="subtitle mb-2">Frontend Developer</h6>
+                                <ul class="list-inline mb-1">
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-facebook-square"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-twitter-square"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-instagram-square"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-linkedin"></i></a></li>
+                                </ul>
+                                <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 mb-4">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img src="{{ asset('assets/IMG20210522163312.jpg')}}" alt="wrapkit" class="img-fluid rounded-circle"/>
+                        </div>
+                        <div class="col-md-12 text-center">
+                            <div class="pt-2">
+                                <h5 class="mt-4 font-weight-medium mb-0">Arif Hendrawan Priliyanto</h5>
+                                <h6 class="subtitle mb-2">Frontend Developer</h6>
+                                <ul class="list-inline mb-1">
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-facebook-square"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-twitter-square"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-instagram-square"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-linkedin"></i></a></li>
+                                </ul>
+                                <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 mb-4">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img src="{{ asset('assets/IMG20210522163312.jpg')}}" alt="wrapkit" class="img-fluid rounded-circle"/>
+                        </div>
+                        <div class="col-md-12 text-center">
+                            <div class="pt-2">
+                                <h5 class="mt-4 font-weight-medium mb-0">Arif Hendrawan Priliyanto</h5>
+                                <h6 class="subtitle mb-2">Frontend Developer</h6>
+                                <ul class="list-inline mb-1">
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-facebook-square"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-twitter-square"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-instagram-square"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-linkedin"></i></a></li>
+                                </ul>
+                                <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 mb-4">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img src="{{ asset('assets/IMG20210522163312.jpg')}}" alt="wrapkit" class="img-fluid rounded-circle"/>
+                        </div>
+                        <div class="col-md-12 text-center">
+                            <div class="pt-2">
+                                <h5 class="mt-4 font-weight-medium mb-0">Arif Hendrawan Priliyanto</h5>
+                                <h6 class="subtitle mb-2">Frontend Developer</h6>
+                                <ul class="list-inline mb-1">
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-facebook-square"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-twitter-square"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-instagram-square"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa-brands fa-linkedin"></i></a></li>
+                                </ul>
+                                <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
